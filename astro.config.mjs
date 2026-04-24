7cd8683ab570661489b311ee782136b5483f72a2
lxs <l4973738@gmail.com>
2026-04-24 13:56:35 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // 替换成你的 GitHub 用户名和仓库名
+  site: 'https://20040120lxs.github.io',
+  base: '/my_blog', // 如果是部署到主域名（username.github.io），则不需要这行
+});
